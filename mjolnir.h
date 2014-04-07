87f7fd867923b35b04a5103a844cc497ee28e674
denis <denis@debian>
2014-04-07 12:30:39 -0400

    creation of the framework for testing

---
+++ mjolnir.h
@@ -0,0 +1,7 @@
+#ifndef		MJOLNIR_H_
+# define	MJOLNIR_H_
+
+# define true	1
+# define false	0
+
+#endif		/*!MJOLNIR_H_*/
---
+++ test.c
@@ -0,0 +1,8 @@
+#include "mjolnir.h"
+#include "test.h"
+
+
+_Bool	run_tests(void)
+{
+  return true;
+}
